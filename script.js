4af1a7a72663bfd2ea543545e9b629e01f52b90d
taewookim02 <taewookim02@gmail.com>
2022-07-05 01:30:50 +0800

    chore: organizing code

--- script.js
+++ script.js
@@ -2,51 +2,51 @@
 
 const canvas = document.querySelector('.canvas');
 const context = canvas.getContext('2d');
-const color = '#444';
-context.lineWidth = 6;
+const color = '#333';
+context.lineWidth = 4;
 
-// HANGER TOP
-context.beginPath();
-context.moveTo(200, 100);
-context.lineTo(200, 150);
-context.strokeStyle = color;
-context.stroke();
-
-// HANGER TOP HORIZONTAL
-context.beginPath();
-context.moveTo(100, 100);
-context.lineTo(200, 100);
-context.strokeStyle = color;
-context.stroke();
-
-// HANGER LEFT VERTICAL
-context.beginPath();
-context.moveTo(100, 100);
-context.lineTo(100, 400);
-context.strokeStyle = color;
-context.stroke();
-
-// HANGER BOTTOM HORIZONTAL
+// 1. HANGER BOTTOM HORIZONTAL
 context.beginPath();
 context.moveTo(50, 400);
 context.lineTo(300, 400);
 context.strokeStyle = color;
 context.stroke();
 
-// HEAD
+// 2. HANGER LEFT VERTICAL
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(100, 400);
+context.strokeStyle = color;
+context.stroke();
+
+// 3. HANGER TOP HORIZONTAL
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(200, 100);
+context.strokeStyle = color;
+context.stroke();
+
+// 4. HANGER TOP VERTICAL
+context.beginPath();
+context.moveTo(200, 100);
+context.lineTo(200, 150);
+context.strokeStyle = color;
+context.stroke();
+
+// 5. HEAD
 context.beginPath();
 context.fillStyle = color;
 context.arc(200, 175, 25, 0, Math.PI * 2, true);
 context.stroke();
 
-// BODY
+// 6. BODY
 context.beginPath();
 context.moveTo(200, 200);
 context.lineTo(200, 295);
 context.strokeStyle = color;
 context.stroke();
 
-// ARMS
+// 7. ARMS
 context.beginPath();
 context.strokeStyle = color;
 context.moveTo(200, 200);
@@ -55,7 +55,7 @@ context.moveTo(200, 200);
 context.lineTo(250, 230);
 context.stroke();
 
-// LEGS
+// 8. LEGS
 context.beginPath();
 context.strokeStyle = color;
 context.moveTo(200, 295);
